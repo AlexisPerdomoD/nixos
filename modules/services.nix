99e43c18b1c7f7923c2bd2ca7b4814cba945231f
alexisperdomod <dropdibemol@gmail.com>
2025-03-25 07:46:00 -0500

    update: 25 marzo 2025

--- modules/services.nix
+++ modules/services.nix
@@ -4,10 +4,22 @@
   services.flatpak.enable = true;
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = true;
-  services.displayManager.sddm = {
+
+  services.xserver = {
     enable = true;
-    wayland.enable = true;
+    displayManager.defaultSession = "hyprland";
+    displayManager.lightdm.greeters.mini = {
+      enable = true;
+      user = "sixela";
+      extraConfig = ''
+        [greeter]
+        show-password-label = true;
+        [greeter-theme]
+        background-image = ""
+      '';
+    };
+    windowManager = {
+      hypr.enable = true;
+    };
   };
-  # services.xserver.desktopManager.openbox.enable = true;
-  # services.displayManager.ly.enable = true;
 }
